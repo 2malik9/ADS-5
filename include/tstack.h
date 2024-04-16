1a4aa977f003b67936f2d63e6e2f8bcdb7281064
2malik9 <160342518+2malik9@users.noreply.github.com>
2024-04-16 19:17:14 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,50 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template <typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T stack[size];
+    int top;
+
+public:
+    TStack() : top(-1) {}
+
+    bool isFull() {
+        return top == size - 1;
+    }
+
+    bool isEmpty() {
+        return top == -1;
+    }
+
+    void push(T value) {
+        if (!isFull()) {
+            top++;
+            stack[top] = value;
+        } else {
+            throw std::overflow_error("Stack is full");
+        }
+    }
+
+    T pop() {
+        if (!isEmpty()) {
+            T value = stack[top];
+            top--;
+            return value;
+        } else {
+            throw std::underflow_error("Stack is empty");
+        }
+    }
+
+    T get() {
+        if (!isEmpty()) {
+            return stack[top];
+        } else {
+            throw std::underflow_error("Stack is empty");
+        }
+    }
 };
+
 
 #endif  // INCLUDE_TSTACK_H_
